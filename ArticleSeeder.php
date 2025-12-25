009048ce752d217b59593e1cd97128f45bba580f
Om Aman Srivastava <osrivastava50_be23@thapar.edu>
2025-12-25 14:17:11 +0530

    Add files via upload

---
+++ ArticleSeeder.php
@@ -0,0 +1,11 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+
+class ArticleSeeder extends Seeder {
+    public function run(): void {
+        Article::factory()->count(5)->create();
+    }
+}
